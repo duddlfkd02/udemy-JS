44adcb9a683d3c61be3ae7294c7f9d2c645351f4
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-06-08 11:15:39 +0900

    함수, 매개변수, 인수

---
+++ Basic/app.js
@@ -0,0 +1,16 @@
+function singSong() {
+    console.log('do');
+    console.log('re');
+    console.log('mi');
+}
+function greet(firstName) {
+    console.log(`Hey there, ${firstName}`)
+    // console.log(`firstName is: ${firstName}`)
+    // console.log('hi');
+}
+
+//firstName으로 정의한 것은 = 매개변수
+
+//매개변수와 인수는 식별 능력이 없다. 전달한 걸 그대로 저장할 뿐!
+//전달 받은 인수가 없으면 에러가 아니라 'undefined'
+
