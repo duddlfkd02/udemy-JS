aa7faa91c78e4013ba670ea9d034bd6a46fabf57
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-06-13 09:07:42 +0900

    async, await

--- AsyncFunctions/app.js
+++ AsyncFunctions/app.js
@@ -1,42 +1,35 @@
 // async function hello() {
 // }
 
+// const sing = async () => {
+//     throw 'ah!';
+//     return 'LA LA LA LA';
+// }
 
-const sing = async () => {
-    throw "OH NO, PROBLEM!"
-    return 'LA LA LA LA'
-}
-
-sing()
-    .then(data => {
-        console.log("PROMISE RESOLVED WITH:", data)
-    })
-    .catch(err => {
-        console.log("OH NO, PROMISE REJECTED!")
-        console.log(err)
-    })
-
-
-
+// sing()
+//     .then((data) => {
+//         console.log('Promise resoleved with', data);
+//     })
+//     .catch((err) => {
+//         console.log('Error');
+//         console.log(err);
+//     })
 
 const login = async (username, password) => {
     if (!username || !password) throw 'Missing Credentials'
-    if (password === 'corgifeetarecute') return 'WELCOME!'
+    if (password === 'corgifeedarecute') return 'WELCOME!'
     throw 'Invalid Password'
 }
 
-login('todd', 'corgifeetarecute')
+login('dsdsds', 'corgifeedarecute')
     .then(msg => {
-        console.log("LOGGED IN!")
-        console.log(msg)
+        console.log('Loged in!');
+        console.log(msg);
     })
-    .catch(err => {
-        console.log("ERROR!")
-        console.log(err)
+    .catch((err) => {
+        console.log('ERROR');
+        console.log(err);
     })
-
-
-
 
 
 
@@ -49,37 +42,46 @@ const delayedColorChange = (color, delay) => {
     })
 }
 
-// delayedColorChange('red', 1000)
-//     .then(() => delayedColorChange('orange', 1000))
-//     .then(() => delayedColorChange('yellow', 1000))
-//     .then(() => delayedColorChange('green', 1000))
-//     .then(() => delayedColorChange('blue', 1000))
-//     .then(() => delayedColorChange('indigo', 1000))
-//     .then(() => delayedColorChange('violet', 1000))
-
+// delayedColorChange('tomato', 1000)
+//     .then(() => {
+//         return delayedColorChange('orange', 1000)
+//     })
+//     .then(() => {
+//         return delayedColorChange('yellow', 1000)
+//     })
+//     .then(() => {
+//         return delayedColorChange('green', 1000)
+//     })
+//     .then(() => {
+//         return delayedColorChange('indigo', 1000)
+//     })
 
 async function rainbow() {
-    await delayedColorChange('red', 1000)
-    await delayedColorChange('orange', 1000)
-    await delayedColorChange('yellow', 1000)
-    await delayedColorChange('green', 1000)
-    await delayedColorChange('blue', 1000)
-    await delayedColorChange('indigo', 1000)
-    await delayedColorChange('violet', 1000)
-    return "ALL DONE!"
+    await delayedColorChange('tomato', 1000);
+    await delayedColorChange('orange', 1000);
+    await delayedColorChange('yellow', 1000);
+    await delayedColorChange('green', 1000);
+    await delayedColorChange('blue', 1000);
+    return 'ALL DONE!!';
 }
 
-// rainbow().then(() => console.log("END OF RAINBOW!"))
+//결과값 출력 방법 1.
+// rainbow().then(() => console.log('End of rainbow'));
 
-
-async function printRainbow() {
+//결과값 출력 방법 2.
+async function printRaninbow() {
     await rainbow();
-    console.log("END OF RAINBOW!")
+    console.log('End of rainbow!');
 }
+//>> await 키워드가 Promise 를 기다리게 하고 모두 끝날 때까지 기다린 후에 Promise 반환됨
 
-printRainbow();
+printRaninbow();
 
-const fakeRequest = (url) => {
+
+
+/* 오류(에러)가 났을 때 */
+
+const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
         const delay = Math.floor(Math.random() * (4500)) + 500;
         setTimeout(() => {
@@ -95,13 +97,12 @@ const fakeRequest = (url) => {
 
 async function makeTwoRequests() {
     try {
-        let data1 = await fakeRequest('/page1');
+        let data1 = await fakeRequestPromise('/page1');
         console.log(data1);
-        let data2 = await fakeRequest('/page2');
+        let data2 = await fakeRequestPromise('/page2');
         console.log(data2);
-    } catch (e) {
-        console.log("CAUGHT AN ERROR!")
-        console.log("error is:", e)
+    } catch (er) {
+        console.log('ERROR!!!')
+        console.log('ERROR is:', er)
     }
-
 }
