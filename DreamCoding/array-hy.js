91f32ea9fa70e6b7e4793e0f5f956c2d17659b29
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-06-23 16:25:18 +0900

    array 복습

---
+++ DreamCoding/array-hy.js
@@ -0,0 +1,83 @@
+// Q1. make a string out of an array
+{
+  const fruits = ['apple', 'banana', 'orange'];
+  const str = fruits.toString();
+  console.log(str);
+}
+
+// Q2. make an array out of a string
+{
+  const fruits = '🍎, 🥝, 🍌, 🍒';
+  const arr = fruits.split();
+  console.log(arr);
+}
+
+// Q3. make this array look like this: [5, 4, 3, 2, 1]
+{
+  const array = [1, 2, 3, 4, 5];
+  const result = array.reverse();
+  console.log(result);
+}
+
+// Q4. make new array without the first two elements
+{
+  const array = [1, 2, 3, 4, 5];
+  const newArr = array.splice(2);
+  console.log(newArr);
+}
+
+class Student {
+  constructor(name, age, enrolled, score) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.score = score;
+  }
+}
+const students = [
+  new Student('A', 29, true, 45),
+  new Student('B', 28, false, 80),
+  new Student('C', 30, true, 90),
+  new Student('D', 40, false, 66),
+  new Student('E', 18, true, 88),
+];
+
+// Q5. find a student with the score 90
+{
+  students.forEach((student) => console.log(student.score === 90));
+}
+
+// Q6. make an array of enrolled students
+{
+}
+
+// Q7. make an array containing only the students' scores
+// result should be: [45, 80, 90, 66, 88]
+{
+}
+
+// Q8. check if there is a student with the score lower than 50
+{
+}
+
+// Q9. compute students' average score
+{
+  for (let num of score) {
+    let sum = 0;
+    console.log(sum);
+    sum += num;
+
+    let result = sum / 5;
+    console.log(result);
+  }
+}
+
+// Q10. make a string containing all the scores
+// result should be: '45, 80, 90, 66, 88'
+{
+}
+
+// Bonus! do Q10 sorted in ascending order
+// result should be: '45, 66, 80, 88, 90'
+{
+}
